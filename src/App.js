f5fc9743df210bd4e4a05ef7755aa9c3860a08fe
93virus <bikasadhikari333@gmail.com>
2021-09-23 04:43:45 +0530

    prot

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {useEffect, useState} from 'react';
 import {nanoid} from 'nanoid';
 import io from 'socket.io-client';
 
-const socket = io("http://chatjsbackend.herokuapp.com:5000/");
+const socket = io("http://chatjsbackend.herokuapp.com/socket.io/?EIO=4&transport=websocket");
 let username = nanoid(5);
 
 function App() {
